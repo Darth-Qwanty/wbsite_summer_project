3862e72fdce458adf345983449be13b480863513
Darth-Qwanty <oleks.study.work@gmail.com>
2025-08-29 19:48:49 +0100

    Add files via upload

---
+++ music.js
@@ -0,0 +1,26 @@
+let songs =[
+    {
+        name: 'Good Stalker',
+        path: 'music/Good_Stalker.mp3',
+        artist: "Unknown Stalker",
+        cover: 'images/Stalker.jpg',
+    },
+    {
+        name: 'Umapyoi',
+        path: 'music/y2mate--ウマ娘-うまぴょい伝説-パート分けColor-CodedLyrics-Umapyoi-Legend馬兒蹦跳傳說.mp3',
+        artist: "Cygames",
+        cover: 'images/umamusume.webp',
+    },
+    {
+        name: 'Blach Hole Sun',
+        path: 'music/y2mate--Soundgarden-Black-Hole-Sun.mp3',
+        artist: "Soundgarden",
+        cover: 'images/Blach hole sun.jpg',
+    },
+    {
+        name: 'Numb',
+        path: 'music/y2mate--Linkin-Park-Numb-Lyrics.mp3',
+        artist: "Linkin Park",
+        cover: 'images/Numb.jpg',
+    }
+]
